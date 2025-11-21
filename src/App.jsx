4360951485cf2f8561c5b9f6a1e9f9d5cae6aea9
Ximena Alvarez <xalvarezmontoy00@mylangara.ca>
2025-11-20 18:07:23 -0800

    Sign in and sign out

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Feedback from './pages/Feedback.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import MonthlySchedule from './pages/MonthlySchedule.jsx';
 import Profile from './pages/Profile.jsx';
 
@@ -12,10 +13,11 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/monthly-schedule" element={<MonthlySchedule />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard/:operatorId" element={<Dashboard />} />
+        <Route path="/profile/:operatorId" element={<Profile />} />
+        <Route path="/feedback/:operatorId" element={<Feedback />} />
+        <Route path="/monthly-schedule/:operatorId" element={<MonthlySchedule />} />
       </Routes>
     </Router>
   );
